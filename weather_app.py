d8504b8113aec77ef5333355ec3b8301d3c9645c
Nino Dakov <71885756+ndakov@users.noreply.github.com>
2023-08-10 09:50:21 +0200

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -23,7 +23,7 @@ import requests
 import json
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
-from datetime import datetime
+from datetime import datetime, timedelta
 from datetime import timezone as tmz
 import pytz
 from tzwhere import tzwhere
@@ -145,7 +145,7 @@ with st.spinner('Loading...'):
     
     time_now = datetime.now(tmz.utc)+tzoffset
     
-    fig.add_vline(x = time_now, line_color="red", opacity=0.4)
+    fig.add_vline(x = time_now+timedelta(days=1,hours=2,minutes=0), line_color="red", opacity=0.4)
     fig.add_annotation(x = time_now, y=max(hourly_df['Temperature °C'])+5,
                 text = time_now.strftime("%d %B %y, %H:%M"),
                 showarrow=False,
