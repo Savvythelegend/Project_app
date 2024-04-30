ab30449f681984b92db0e72fea1db8c785a57c61
Savvy <162735469+Savvythelegend@users.noreply.github.com>
2024-04-30 12:54:47 +0530

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -200,7 +200,7 @@ with st.spinner('Loading...'):
     # Concluding remarks
     st.write('Weather data source: [http://open-meteo.com](http://open-meteo.com) \n\n'+
              'List of 40,000+ world cities: [https://simplemaps.com/data/world-cities](https://simplemaps.com/data/world-cities) \n\n' +
-             'Github repository: [streamlit-weather-app](https://github.com/Savvythelegend/weather-app)')
+             'Github repository: [streamlit-weather-app](https://github.com/Savvythelegend/Project_app)')
     st.write('Thanks for stopping by. Cheers!')
 
 
