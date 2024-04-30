a71b8cd23d76fa0dd2bd664958e8a385d8da2f64
Mehfooj Alam <112606544+Mehfooj-glitch@users.noreply.github.com>
2024-04-30 12:39:17 +0530

    app.py
    This project has been successfully delivered.

--- weather_app.py
+++ weather_app.py
@@ -200,7 +200,7 @@ with st.spinner('Loading...'):
     # Concluding remarks
     st.write('Weather data source: [http://open-meteo.com](http://open-meteo.com) \n\n'+
              'List of 40,000+ world cities: [https://simplemaps.com/data/world-cities](https://simplemaps.com/data/world-cities) \n\n' +
-             'Github repository: [streamlit-weather-app](https://github.com/ndakov/streamlit-weather-app)')
+             'Github repository: [streamlit-weather-app](https://github.com/Savvythelegend/weather-app)')
     st.write('Thanks for stopping by. Cheers!')
 
 
